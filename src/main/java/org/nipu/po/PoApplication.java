b6ced879983c740036bed46b09a7d13bf95140c0
Nikita Puzankov <humb1t@yandex.ru>
2018-06-18 19:01:41 +0400

    Working monolith application

---
+++ src/main/java/org/nipu/po/PoApplication.java
@@ -0,0 +1,12 @@
+package org.nipu.po;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoApplication.class, args);
+    }
+}
