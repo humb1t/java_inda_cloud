d4c7dc934914669c50a82d092f8739e119c93efc
Nikita Puzankov <humb1t@yandex.ru>
2018-06-20 00:45:25 +0400

    Working minikube configuration

--- po/src/main/java/org/nipu/po/PoApplication.java
+++ po/src/main/java/org/nipu/po/PoApplication.java
@@ -3,12 +3,10 @@ package org.nipu.po;
 import org.nipu.po.order.clients.ProductSpecificationRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(clients = ProductSpecificationRepository.class)
-@EnableDiscoveryClient
 public class PoApplication {
 
     public static void main(String[] args) {
